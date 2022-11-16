dcc543e141c5a658d183e09be50dd7b204e4ca06
Arya Emami <aryaemami59@yahoo.com>
2022-11-16 17:47:06 -0600

    fixed eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "aos/dist/aos.css";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+const container = document.getElementById("root") as HTMLDivElement;
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
