9fd2b0f3420f269d36b08ba563e94e2d4a639382
Arya Emami <aryaemami59@yahoo.com>
2023-01-15 17:16:23 -0600

    updated dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
-    open: "http://127.0.0.1:3000/",
+    open: "http://localhost:3000/",
   },
   base: "/portfolio/",
   plugins: [react()],
